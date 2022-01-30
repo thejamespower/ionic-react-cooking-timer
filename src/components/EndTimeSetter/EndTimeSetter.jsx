d58d6144cbd182fe5f2b34ebe24706d213f4dc78
James Power <thejamespower@gmail.com>
2022-01-30 15:42:49 +0000

    Make EndTimeSetter FC

--- src/components/EndTimeSetter/EndTimeSetter.jsx
+++ src/components/EndTimeSetter/EndTimeSetter.jsx
@@ -1,55 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { IonButton, IonItem, IonLabel } from '@ionic/react';
 import CustomTimeField from '../TimeField';
 
-const initialState = {
-  endTime: '19:00:00',
+const initialState = '19:00:00';
+
+const EndTimeSetter = ({ setEndTime: setEndTimeAction }) => {
+  const [endTime, setEndTime] = useState(initialState);
+
+  return (
+    <IonItem>
+      <IonLabel>End time</IonLabel>
+      <CustomTimeField value={endTime} onChange={(x) => setEndTime(x)} />
+      <IonButton
+        color="primary"
+        expand="block"
+        onClick={() => {
+          setEndTimeAction(endTime);
+        }}
+      >
+        Set end time
+      </IonButton>
+    </IonItem>
+  );
 };
 
-class EndTimeSetter extends Component {
-  static propTypes = {
-    setEndTime: PropTypes.func.isRequired,
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = initialState;
-
-    this.changeEndTime = this.changeEndTime.bind(this);
-    this.addEndTime = this.addEndTime.bind(this);
-  }
-
-  changeEndTime(endTime) {
-    this.setState({ endTime });
-  }
-
-  addEndTime() {
-    const { setEndTime } = this.props;
-    const { endTime } = this.state;
-    setEndTime(endTime);
-  }
-
-  render() {
-    const { endTime } = this.state;
-
-    return (
-      <IonItem>
-        <IonLabel>End time</IonLabel>
-        <CustomTimeField value={endTime} onChange={this.changeEndTime} />
-        <IonButton
-          color="primary"
-          expand="block"
-          onClick={() => {
-            this.addEndTime(endTime);
-          }}
-        >
-          Set end time
-        </IonButton>
-      </IonItem>
-    );
-  }
-}
+EndTimeSetter.propTypes = {
+  setEndTime: PropTypes.func.isRequired,
+};
 
 export default EndTimeSetter;
